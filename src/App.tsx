4f0362c0261e90f09ceb11d43d9c6005d03e636e
Vitor Pereira de Oliveira <vitopereira@discente.ufg.br>
2021-06-20 21:55:16 -0300

    :sparkles: Add eslint and prettier configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 const App: React.FC = () => {
   return <h1>Hello Let Me Ask</h1>;
-}
+};
 
 export default App;
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import firebase from 'firebase/app';
 
 import 'firebase/analytics';
